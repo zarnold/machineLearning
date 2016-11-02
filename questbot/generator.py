2e05eeac77154e14fb9dcc9ade24e286fefc61a6
zarnold <zarnold@9online.fr>
2016-11-02 22:33:41 +0100

    new generator for Fauxbert

---
+++ questbot/generator.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import pickle
+
+import tflearn
+from tflearn.data_utils import *
+
+DATA_PATH="/home/arnold/Data/datasets-text/"
+path = "classiques.txt"
+
+maxlen = 25
+
+char_idx = None
+
+X, Y, char_idx =   textfile_to_semi_redundant_sequences(DATA_PATH + path, seq_maxlen=maxlen, redun_step=3)
+
+
+
+g = tflearn.input_data([None, maxlen, len(char_idx)])
+g = tflearn.lstm(g, 512, return_seq=True)
+g = tflearn.dropout(g, 0.5)
+g = tflearn.lstm(g, 512, return_seq=True)
+g = tflearn.dropout(g, 0.5)
+g = tflearn.lstm(g, 512)
+g = tflearn.dropout(g, 0.5)
+g = tflearn.fully_connected(g, len(char_idx), activation='softmax')
+g = tflearn.regression(g, optimizer='adam', loss='categorical_crossentropy',
+                       learning_rate=0.001)
+
+m = tflearn.SequenceGenerator(g, dictionary=char_idx,
+                              seq_maxlen=maxlen,
+                              clip_gradients=5.0,
+                              checkpoint_path='classiques')
+
+def talk():
+  seed = random_sequence_from_textfile(DATA_PATH + path, maxlen)
+  ph=m.generate(2000,temperature=0.7, seq_seed=seed)
+  print(ph)
+  with open("Flauxbert.txt","a") as fh:
+    fh.write('========================\n')
+    fh.write(ph+'\n')
+  seed="Je pense que la vie est une "[:maxlen]
+  ph=m.generate(200,temperature=0.7, seq_seed=seed)
+  with open("Flauxbert.txt","a") as fh:
+    fh.write('===\n')
+    fh.write(ph+'\n')
+
+
+
+for i in range(50):
+    m.fit(X, Y, validation_set=0.1, batch_size=128, n_epoch=1, run_id='classiques')
+    talk()
+
+
+
+
--- questbot/word2vec.py
+++ questbot/word2vec.py
@@ -1,29 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import gensim
+import gensim, logging
+import numpy as np
+import pandas as pd
+from sklearn.manifold import TSNE
 import re
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-PATH  = "/home/arnold/Data/datasets-text/word2vec/"
-source  = "news"
+PATH  = "/home/arnold/Data/datasets-text/"
+#source  = "news"
+source  = "classiques"
 
 with open(PATH+source+".txt","r") as fh:
-  text = fh.read().split("\n")
+  text = fh.read().lower().split("\n")
 
 
-sentences = map(lambda s: re.split("[, \-!?:.']", s),text)
+sentences = map(lambda s: re.split("[(), \-!?:.']", s),text)
 model = gensim.models.Word2Vec(
   sentences,
-  min_count = 5,
+  min_count = 50,
   sg = 0,
   size = 100
 )
+model.save('./models/'+source+'.wv')
 
-for w in model.vocab:
-  print w
+#########################################################################
 
-with open("./models/"+source+".wv","w") as fname:
-  model.save(fname)
+#model = gensim.models.Word2Vec.load('./models/'+source+'.wv')
+
+## build array of vector
+X=model.syn0
+
+## Get coutn of each word
+word = []
+count= []
+
+for k in model.vocab:
+  word.append(k)
+  count.append(model.vocab[k].count)
+
+idx = np.argsort(count)
+ranked_w = np.array(word)[idx]
+
+
+## Get top words
+limit =10000
+top_w = ranked_w[-1*limit:]
+
+x=[]
+for w in top_w:
+  x.append(model[w])
+
+## Visualisation
+x=np.array(x)
+projection = TSNE(n_components=2, random_state=42)
+p = projection.fit_transform(x) 
+df  =pd.DataFrame(p,index= top_w)
+df.to_csv(source+'_vec.csv')
+
+
+def tellme(word):
+  for w in model.most_similar(word):
+    print w[0]
+
+
+
+
+
 
 
 

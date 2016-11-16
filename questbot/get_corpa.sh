da9f5111f731abaeb3928a5662e8be68d7308d5f
zarnold <zarnold@9online.fr>
2016-11-16 18:48:35 +0100

    how to get corpa

--- questbot/get_corpa.sh
+++ questbot/get_corpa.sh
@@ -1,4 +1,12 @@
-# This script get and make frenchtext dataset from news corpus
+###############################################################################################
+#
+# Script for training good word and phrase vector model using public corpora, version 1.0.
+# The training time will be from several hours to about a day.
+#
+# Downloads about 8 billion words, makes phrases using two runs of word2phrase, trains
+# a 500-dimensional vector model and evaluates it on word and phrase analogy tasks.
+#
+###############################################################################################
 
 # This function will convert text to lowercase and remove special characters
 normalize_text() {
@@ -8,9 +16,59 @@ normalize_text() {
   -e 's/«/ /g' | tr 0-9 " "
 }
 
+mkdir word2vec
+cd word2vec
+
 wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2012.fr.shuffled.gz
 wget http://www.statmt.org/wmt14/training-monolingual-news-crawl/news.2013.fr.shuffled.gz
 gzip -d news.2012.fr.shuffled.gz
 gzip -d news.2013.fr.shuffled.gz
 normalize_text < news.2012.fr.shuffled > data.txt
 normalize_text < news.2013.fr.shuffled >> data.txt
+
+wget http://dumps.wikimedia.org/frwiki/latest/frwiki-latest-pages-articles.xml.bz2
+bzip2 -c -d frwiki-latest-pages-articles.xml.bz2 | awk '{print tolower($0);}' | perl -e '
+# Program to filter Wikipedia XML dumps to "clean" text consisting only of lowercase
+# letters (a-z, converted from A-Z), and spaces (never consecutive)...
+# All other characters are converted to spaces.  Only text which normally appears.
+# in the web browser is displayed.  Tables are removed.  Image captions are.
+# preserved.  Links are converted to normal text.  Digits are spelled out.
+# *** Modified to not spell digits or throw away non-ASCII characters ***
+
+# Written by Matt Mahoney, June 10, 2006.  This program is released to the public domain.
+
+$/=">";                     # input record separator
+while (<>) {
+  if (/<text /) {$text=1;}  # remove all but between <text> ... </text>
+  if (/#redirect/i) {$text=0;}  # remove #REDIRECT
+  if ($text) {
+
+    # Remove any text not normally visible
+    if (/<\/text>/) {$text=0;}
+    s/<.*>//;               # remove xml tags
+    s/&amp;/&/g;            # decode URL encoded chars
+    s/&lt;/</g;
+    s/&gt;/>/g;
+    s/<ref[^<]*<\/ref>//g;  # remove references <ref...> ... </ref>
+    s/<[^>]*>//g;           # remove xhtml tags
+    s/\[http:[^] ]*/[/g;    # remove normal url, preserve visible text
+    s/\|thumb//ig;          # remove images links, preserve caption
+    s/\|left//ig;
+    s/\|right//ig;
+    s/\|\d+px//ig;
+    s/\[\[image:[^\[\]]*\|//ig;
+    s/\[\[category:([^|\]]*)[^]]*\]\]/[[$1]]/ig;  # show categories without markup
+    s/\[\[[a-z\-]*:[^\]]*\]\]//g;  # remove links to other languages
+    s/\[\[[^\|\]]*\|/[[/g;  # remove wiki url, preserve visible text
+    s/{{[^}]*}}//g;         # remove {{icons}} and {tables}
+    s/{[^}]*}//g;
+    s/\[//g;                # remove [ and ]
+    s/\]//g;
+    s/&[^;]*;/ /g;          # remove URL encoded chars
+
+    $_=" $_ ";
+    chop;
+    print $_;
+  }
+}
+' | normalize_text | awk '{if (NF>1) print;}' >> wikipedia.txt
